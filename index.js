2702a53f7da6f3171f096b5c5fb227e661906482
Guilherme Taira <gtaira@nelogica.com.br>
2023-06-26 21:33:55 -0300

    Paginação

--- index.js
+++ index.js
@@ -21,157 +21,185 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
-app.listen(21262, () => {
-  console.log('Express started at http://localhost:21262')
-})
+app.post('/api/v1/pedido', async (req, res) => {
+  try {
+    const { numero, cliente } = req.body;
 
-app.post('/api/v1/pedido', (req, res) => {
-  const novoPedido = req.body;
-  admin.firestore()
-    .collection('pedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      if (novoPedido && novoPedido.numero && novoPedido.cliente && !pedidos.find((pedido) => pedido.numero.toString() === novoPedido.numero.toString())) {
-        const pedido = {
-          numero: novoPedido.numero,
-          cliente: novoPedido.cliente,
-        }
-        db.collection('pedido').doc().set(pedido)
-          .then(() => {
-            return res.send({ status: 'OK', response: 'Pedido inserido com sucesso!' });
-          }).catch(() => {
-            return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-          });
-      } else {
-        return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-      }
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-    });
+    const pedidoQuery = await db.collection('pedido').where('numero', '==', numero).get();
 
-})
+    if (!pedidoQuery.empty) {
+      return res.status(400).json({ message: 'Pedido já existe.' });
+    }
 
-app.get('/api/v1/pedido/item', (req, res) => {
-  const produto = req.query.produto;
-  if (produto && produto !== '') {
-    admin.firestore()
-    .collection('itemPedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      })).filter((item) => item.produto.toLowerCase().includes(produto.toLowerCase()));
-      return res.send(pedidos || [])
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-    });
+    const novoPedidoRef = await db.collection('pedido').add({ numero, cliente });
+
+    return res.status(201).json({ message: 'Pedido inserido com sucesso.', id: novoPedidoRef.id });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
   }
-  return [];
-})
+});
 
-app.get('/api/v1/pedido/:nPedido', (req, res) => {
-  const nPedido = req.params.nPedido;
-  admin.firestore()
-    .collection('pedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      })).find((pedido) => pedido.numero.toString() === nPedido.toString())
-      return res.send(pedidos || {})
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-    });
-})
+app.get('/api/v1/pedido/item', async (req, res) => {
+  try {
+    const produtoQuery = req.query.produto;
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
 
-app.get('/api/v1/pedido', (req, res) => {
-  admin.firestore()
-    .collection('pedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      return res.send(pedidos)
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-    });
-})
+    const itemPedidoRef = db.collection('itemPedido');
 
-app.post('/api/v1/pedido/:nPedido/item', (req, res) => {
-  const nPedido = req.params.nPedido;
-  admin.firestore()
-    .collection('pedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      if(nPedido && pedidos.find((pedido) => pedido.numero.toString() === nPedido.toString())) {
-        const novoItem = req.body;
-        if(novoItem && novoItem.indice && novoItem.sku && novoItem.produto && novoItem.preco && novoItem.quantidade) {
-          const novoItemPedido = {
-            numero: nPedido,
-            indice: novoItem.indice,
-            sku: novoItem.sku,
-            produto: novoItem.produto,
-            preco: novoItem.preco,
-            quantidade: novoItem.quantidade,
-          }
-          db.collection('itemPedido').doc().set(novoItemPedido)
-          .then(() => {
-            return res.send({ status: 'OK', response: 'Pedido inserido com sucesso!' });
-          }).catch(() => {
-            return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-          });
-        } else {
-          return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao cadastrar um pedido!' });
-        }
-      } else {
-        return res.send({ status: 'ERRO', response: 'Número do pedido não encontrado!' });
-      }
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
-    });
-})
+    const snapshot = await itemPedidoRef
+      .where('produto', '>=', produtoQuery)
+      .limit(perPage)
+      .offset((page - 1) * perPage)
+      .get();
 
-app.get('/api/v1/pedido/:nPedido/item/:indiceItem', (req, res) => {
-  const nPedido = req.params.nPedido;
-  const indiceItem = req.params.indiceItem;
-  admin.firestore()
-    .collection('itemPedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      })).filter((item) => item.indice.toString() === indiceItem.toString() && item.numero.toString() === nPedido.toString());
-      return res.send(pedidos)
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
+    const itensPedido = [];
+    snapshot.forEach(doc => {
+      const itemPedido = doc.data();
+      const id = doc.id;
+      itensPedido.push({ ...itemPedido, id });
     });
-})
 
-app.get('/api/v1/pedido/:nPedido/item', (req, res) => {
-  const nPedido = req.params.nPedido;
-  admin.firestore()
-    .collection('itemPedido')
-    .get()
-    .then(snapshot => {
-      const pedidos = snapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data()
-      })).filter((item) => item.numero.toString() === nPedido.toString());
-      return res.send(pedidos || [])
-    }).catch(() => {
-      return res.send({ status: 'ERRO', response: 'Ocorreu um erro ao inserir seu pedido!' });
+    return res.status(200).json({ itensPedido, page, total: snapshot.size });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+app.get('/api/v1/pedido/:numero', async (req, res) => {
+  try {
+    const numero = req.params.numero;
+
+    const pedidoQuery = await db.collection('pedido').where('numero', '==', parseInt(numero)).get();
+
+    if (pedidoQuery.empty) {
+      return res.status(404).json({ message: 'Pedido não encontrado.' });
+    }
+
+    const pedido = pedidoQuery.docs[0].data();
+    const id = pedidoQuery.docs[0].id;
+
+    return res.status(200).json({ ...pedido, id });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+app.get('/api/v1/pedido', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
+
+    const pedidosRef = db.collection('pedido');
+
+    const snapshot = await pedidosRef.limit(perPage).offset((page - 1) * perPage).get();
+
+    const pedidos = [];
+    snapshot.forEach(doc => {
+      const pedido = doc.data();
+      const id = doc.id;
+      pedidos.push({ ...pedido, id });
     });
-})
+
+    const totalRegistros = await pedidosRef.get().then(snapshot => snapshot.size);
+
+    const hasNextPage = page * perPage < totalRegistros;
+
+    return res.status(200).json({ pedidos, page, total: totalRegistros, has_next_page: hasNextPage });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+app.post('/api/v1/pedido/:numero/item', async (req, res) => {
+  try {
+    const numeroPedido = req.params.numero;
+    const { indice, sku, produto, preco, quantidade } = req.body;
+    const numeroPedidoParsed = Number(numeroPedido);
+
+    const pedidoQuery = db.collection('pedido').where('numero', '==', numeroPedidoParsed);
+    const pedidoSnapshot = await pedidoQuery.get();
+
+    if (pedidoSnapshot.empty) {
+      return res.status(404).json({ message: 'Pedido não encontrado.' });
+    }
+
+    await db.collection('itemPedido').add({
+      numero: numeroPedidoParsed,
+      indice,
+      sku,
+      produto,
+      preco,
+      quantidade
+    });
+
+    return res.status(201).json({ message: 'Item do pedido adicionado com sucesso.' });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+app.get('/api/v1/pedido/:numero/item/:indice', async (req, res) => {
+  try {
+    const numeroPedido = req.params.numero;
+    const numeroPedidoParsed = Number(numeroPedido);
+    const indiceItem = req.params.indice;
+    const indiceItemParsed = Number(indiceItem);
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
+
+    const itemPedidoRef = db.collection('itemPedido');
+    const query = itemPedidoRef
+      .where('numero', '==', numeroPedidoParsed)
+      .where('indice', '==', indiceItemParsed);
+
+    const snapshot = await query.limit(perPage).offset((page - 1) * perPage).get();
+
+    const itensPedido = [];
+    snapshot.forEach(doc => {
+      const itemPedido = doc.data();
+      const id = doc.id;
+      itensPedido.push({ ...itemPedido, id });
+    });
+
+    return res.status(200).json({ itensPedido, page, total: snapshot.size });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+app.get('/api/v1/pedido/:numero/item', async (req, res) => {
+  try {
+    const numeroPedido = Number(req.params.numero);
+    const page = parseInt(req.query.page) || 1;
+    const perPage = parseInt(req.query.perPage) || 10;
+
+    const itemPedidoRef = db.collection('itemPedido');
+    const query = itemPedidoRef.where('numero', '==', numeroPedido);
+
+    const snapshot = await query.limit(perPage).offset((page - 1) * perPage).get();
+
+    const itensPedido = [];
+    snapshot.forEach(doc => {
+      const itemPedido = doc.data();
+      const id = doc.id;
+      itensPedido.push({ ...itemPedido, id });
+    });
+
+    return res.status(200).json({ itensPedido, page, total: snapshot.size });
+  } catch (error) {
+    console.error('Erro ao processar a requisição:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
